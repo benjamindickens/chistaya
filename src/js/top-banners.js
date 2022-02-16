0cf75c187b23047128a05d925a3387f76c386651
Boris Mitrofanov <boryamitrofanov@gmail.com>
2022-02-16 16:32:16 +0300

    done

--- src/js/top-banners.js
+++ src/js/top-banners.js
@@ -3,11 +3,13 @@ import 'swiper/swiper-bundle.min.css';
 import {hasClass} from "./common.js";
 
 const btnPerGroup = 5;
-const navigationContainer = document.querySelector(".js-banner-navigation")
+let currentStepWidth = 0;
+const navigationContainer = document.querySelector(".js-banners-navigation");
+const progressBar = document.querySelector(".js-banners-progress");
 let bannerNavigation, bannersSlider = null;
 
-const createNavBtn = (data, btnSlide) => {
-    return `<button data-index="${data.index}" data-slide="${btnSlide}" class="js-banners-nav banners-navigation__btn">
+const createNavBtn = (data, btnSlide, progressStep) => {
+    return `<button data-step="${progressStep}" data-index="${data.index}" data-slide="${btnSlide}" class="js-banners-nav banners-navigation__btn">
 <h3 class="banners-navigation__title">${data.title}</h3>
 <p class="banners-navigation__description">${data.description}</p>
 </button>`
@@ -17,9 +19,22 @@ const setActiveBtn = (selector) => {
     (typeof selector === "number" ? navigationContainer.querySelector(`[data-index="${selector}"]`) : selector).classList.add("_active");
 }
 
+const setStepToSlides = (slider) => {
+    slider.slides.forEach((slide, index) => {
+        const step = (index % btnPerGroup) ;
+        slide.setAttribute("data-step", step)
+    })
+}
+
+const setCurrentProgress = () => {
+    progressBar.style.width = currentStepWidth * +bannersSlider.slides[bannersSlider.realIndex].dataset.step + "%";
+}
+
 const createSlides = (navigationData, btnPerGroup) => {
     const navSlides = [];
     let currentSlide = 0;
+    let progressStep = 0;
+
     navigationData.forEach(data => {
 
         if (!navSlides[currentSlide]) {
@@ -27,11 +42,13 @@ const createSlides = (navigationData, btnPerGroup) => {
         }
 
         if (navSlides[currentSlide].length <= btnPerGroup) {
-            navSlides[currentSlide].push(createNavBtn(data, currentSlide));
+            navSlides[currentSlide].push(createNavBtn(data, currentSlide, progressStep));
+            progressStep++;
         }
 
         if (navSlides[currentSlide].length === btnPerGroup) {
             currentSlide++;
+            progressStep = 0;
             navSlides[currentSlide] = [];
         }
 
@@ -45,16 +62,20 @@ bannersSlider = new Swiper(".js-banner-slider", {
     followFinger: false,
     watchSlidesVisibility: true,
     on: {
+        init() {
+            setStepToSlides(this);
+        },
         slideChangeTransitionStart() {
             navigationContainer.querySelector("._active").classList.remove("_active");
             setActiveBtn(this.realIndex)
             const expectedSlide = navigationContainer.querySelector(`[data-index="${this.realIndex}"]`).dataset.slide;
             bannerNavigation.slideTo(expectedSlide);
+            setCurrentProgress();
         },
     }
 });
 
-bannerNavigation = new Swiper(".js-banner-navigation", {
+bannerNavigation = new Swiper(navigationContainer, {
     followFinger: false,
     watchSlidesVisibility: true,
     virtual: {
@@ -74,9 +95,11 @@ bannerNavigation = new Swiper(".js-banner-navigation", {
     on: {
         init() {
             setActiveBtn(bannersSlider.realIndex);
+            currentStepWidth = 100 / this.slides[this.realIndex].children.length;
         },
         slideChangeTransitionStart() {
             const slideToIndex = this.slides[this.realIndex].firstChild.dataset.index;
+            currentStepWidth = 100 / this.slides[this.realIndex].children.length;
             bannersSlider.slideTo(slideToIndex);
         }
     },
