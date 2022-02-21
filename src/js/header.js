3193b04be86c1f649e6000eaa0bfbc20f7bf04d7
Boris Mitrofanov <boryamitrofanov@gmail.com>
2022-02-21 14:19:51 +0300

    header restiling

--- src/js/header.js
+++ src/js/header.js
@@ -4,23 +4,23 @@ import 'accordion-js/dist/accordion.min.css';
 const menu = document.querySelector(".js-header-menu");
 const dropdowns = document.querySelectorAll(".js-dropdown");
 
-let innerAccordions = null;
-
-
-const mainCollapsesMenu = document.querySelector(".js-nav-main-section")
-const innerCollapsesMenu = [...document.querySelectorAll(".js-nav-inner-section")];
-
-const mainAccordion = new Accordion(mainCollapsesMenu, {
-    duration: 300,
-    showMultiple: false,
-    beforeClose() {
-        innerAccordions.forEach((accordion, index) => {
-            accordion.closeAll();
-        })
-    }
-});
-
-innerAccordions = new Accordion(innerCollapsesMenu, {
-    duration: 300,
-    showMultiple: false,
-});
+// let innerAccordions = null;
+//
+//
+// const mainCollapsesMenu = document.querySelector(".js-nav-main-section")
+// const innerCollapsesMenu = [...document.querySelectorAll(".js-nav-inner-section")];
+//
+// const mainAccordion = new Accordion(mainCollapsesMenu, {
+//     duration: 300,
+//     showMultiple: false,
+//     beforeClose() {
+//         innerAccordions.forEach((accordion, index) => {
+//             accordion.closeAll();
+//         })
+//     }
+// });
+//
+// innerAccordions = new Accordion(innerCollapsesMenu, {
+//     duration: 300,
+//     showMultiple: false,
+// });
