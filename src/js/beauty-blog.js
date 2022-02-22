fc186527af6568486f82a1f135f7b65b10b76751
Boris Mitrofanov <boryamitrofanov@gmail.com>
2022-02-22 20:25:21 +0300

    pagination done in beauty blog

--- src/js/beauty-blog.js
+++ src/js/beauty-blog.js
@@ -1,9 +1,21 @@
 import Swiper from 'swiper/swiper-bundle.min';
 import 'swiper/swiper-bundle.min.css';
+
 const beautyBlogContainer = document.querySelector(".js-blog-slider")
 const beautyBlogSlider = new Swiper(beautyBlogContainer, {
     spaceBetween: 30,
     followFinger: false,
     slidesPerView: 3,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        667: {
+            pagination: false,
+            slidesPerView: 1,
+            spaceBetween: 16,
+        }
+    },
 })
 
