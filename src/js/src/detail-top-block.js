f0502ab03a77aa74708ebea8e2a584ba882b8a4c
Boris Mitrofanov <boryamitrofanov@gmail.com>
2022-02-25 16:14:56 +0300

    block description detail tabs added

--- src/js/src/detail-top-block.js
+++ src/js/src/detail-top-block.js
@@ -1,10 +1,35 @@
 import Swiper from 'swiper/swiper-bundle.min';
 import 'swiper/swiper-bundle.min.css';
-import {getNoun, productCardClickEvents, detectMobile} from "./common.js";
+import {getNoun, hasClass, productCardClickEvents, detectMobile} from "./common.js";
 
 const detailProductContainer = document.querySelector(".js-detail-product-slider");
+
 const previewImg = document.querySelector(".js-product-active-img");
 const sliderSpeed = 300;
+const descriptionTabs = document.querySelector(".js-description-tabs");
+
+const tabsLogic = (e) => {
+    if (hasClass(e.target, null, "js-description-tab-btn")) {
+        let descriptionToClose = null;
+
+        e.target.classList.add("_active");
+        if (e.target.nextElementSibling) {
+            e.target.nextElementSibling.classList.remove("_active");
+            descriptionToClose = e.target.nextElementSibling.dataset.open;
+        } else {
+            e.target.previousElementSibling.classList.remove("_active");
+            descriptionToClose = e.target.previousElementSibling.dataset.open;
+        }
+
+        const elementToClose = document.querySelector(`[data-detail-description="${descriptionToClose}"]`);
+        elementToClose.classList.remove("_active");
+        if (elementToClose.nextElementSibling) {
+            elementToClose.nextElementSibling.classList.add("_active")
+        } else {
+            elementToClose.previousElementSibling.classList.add("_active")
+        }
+    }
+}
 
 const detailProductSlider = new Swiper(detailProductContainer, {
     spaceBetween: 16,
@@ -40,4 +65,6 @@ const detailProductSlider = new Swiper(detailProductContainer, {
             }, sliderSpeed / 2)
         }
     }
-})
+});
+
+descriptionTabs.addEventListener("click", tabsLogic)
