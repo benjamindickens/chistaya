6364978d014124057f2dbf5902da1205d929fd12
Boris Mitrofanov <boryamitrofanov@gmail.com>
2022-02-25 20:08:27 +0300

    bag with pagination fixed

--- src/js/src/detail-top-block.js
+++ src/js/src/detail-top-block.js
@@ -1,27 +1,28 @@
 import Swiper from 'swiper/swiper-bundle.min';
 import 'swiper/swiper-bundle.min.css';
-import {getNoun, hasClass, productCardClickEvents, detectMobile} from "./common.js";
+import {getNoun, productCardClickEvents, detectMobile} from "./common.js";
 
 const detailProductContainer = document.querySelector(".js-detail-product-slider");
 const descriptionCompositionContainer = document.querySelector(".js-composition-slider");
 const tabContainer = document.querySelector(".js-tab");
 const descriptionTabButtons = document.querySelectorAll(".js-description-tab-btn")
-
+const detailProductPag = document.querySelector(".js-detail-main-slider-pag");
 const previewImg = document.querySelector(".js-product-active-img");
 const sliderSpeed = 300;
+const isMobile = detectMobile();
 
 const detailProductSlider = new Swiper(detailProductContainer, {
     spaceBetween: 16,
     followFinger: false,
     slidesPerView: 1,
     speed: 300,
-    slideToClickedSlide: true,
     loop: true,
-    preventInteractionOnTransition: true,
+    slideToClickedSlide: true,
     pagination: {
-        el: '.swiper-pagination',
+        el: '.js-detail-main-slider-pag',
         clickable: true,
     },
+    watchSlidesVisibility: true,
     navigation: {
         nextEl: '.js-detail-main-slider-nav.swiper-button-next',
         prevEl: '.js-detail-main-slider-nav.swiper-button-prev',
@@ -34,14 +35,22 @@ const detailProductSlider = new Swiper(detailProductContainer, {
         }
     },
     on: {
-        transitionStart() {
-            const currentActiveSlide = this.slides[this.realIndex];
-            previewImg.classList.add("_transition")
+        afterInit() {
+            this.transitionStart = () => {
+                const currentActiveSlide = this.slides[this.realIndex];
+                previewImg.classList.add("_transition");
 
-            setTimeout(() => {
-                previewImg.querySelector("img").src = currentActiveSlide.querySelector("img").src;
-                previewImg.classList.remove("_transition")
-            }, sliderSpeed / 2)
+                setTimeout(() => {
+                    previewImg.querySelector("img").src = currentActiveSlide.querySelector("img").src;
+                    previewImg.classList.remove("_transition")
+                }, sliderSpeed / 2);
+
+                if (isMobile) {
+                    detailProductPag.querySelector(".swiper-pagination-bullet-active").classList.remove("swiper-pagination-bullet-active");
+                    detailProductPag.children[this.realIndex].classList.add("swiper-pagination-bullet-active");
+                }
+
+            }
         }
     }
 });
